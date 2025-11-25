ba547117f49566f86395be185215dc08577cf223
jawadahmad2001 <jawadahmad0550@gmail.com>
2025-11-25 14:16:03 +0500

    initial commit

--- app.py
+++ app.py
@@ -506,4 +506,5 @@ def audio_input_page():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
